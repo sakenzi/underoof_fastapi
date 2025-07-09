87ab206eda61bdbb741e0bfb396af277b28ffe66
sakenzi <ermekulysaken8@gmail.com>
2025-07-10 00:00:05 +0600

    Добавлено config

---
+++ database/db.py
@@ -0,0 +1,6 @@
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+from core.config import settings
+from sqlalchemy import create_engine
+
+
