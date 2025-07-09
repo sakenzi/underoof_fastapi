c3323b6fec0c758cc34eaf2975a01f271925ee01
sakenzi <ermekulysaken8@gmail.com>
2025-07-10 00:15:30 +0600

    Добавлено модель пользователя и миграция

--- database/db.py
+++ database/db.py
@@ -4,3 +4,29 @@ from core.config import settings
 from sqlalchemy import create_engine
 
 
+async_engine = create_async_engine(
+    url=settings.DATABASE_URL_asyncpg,
+    echo=False
+)
+
+async_session_factory = async_sessionmaker(
+    bind=async_engine,
+    expire_on_commit=False
+)
+
+sync_engine = create_engine(
+    url=settings.DATABASE_URL_psycopg2,  
+    echo=False
+)
+
+SessionLocal = sessionmaker(
+    bind=sync_engine,
+    expire_on_commit=False
+)
+
+class Base(DeclarativeBase):
+    pass
+
+async def get_db():
+    async with async_session_factory() as session:
+        yield session
---
+++ model/models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import String, Integer, Text, Column,  func, DateTime, ForeignKey, Float, Boolean
+from sqlalchemy.orm import relationship
+from database.db import Base
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), nullable=True)
+    phone_number = Column(String(20), unique=True, index=True)
+    password = Column(Text, nullable=True)
+    code = Column(String, nullable=True)
+    is_verified = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), default=func.now(), nullable=False)
