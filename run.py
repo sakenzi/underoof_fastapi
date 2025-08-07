5ebab5bbbef939d2d3645bf6b0ff94f5eeaf4418
sakenzi <ermekulysaken8@gmail.com>
2025-08-07 16:32:54 +0500

    Добавлено поиск по ближайщим точкам

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
-    # uvicorn.run("main:app", host="192.168.8.102", port=8000, reload=True)
+    # uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main:app", host="192.168.8.105", port=8000, reload=True)
